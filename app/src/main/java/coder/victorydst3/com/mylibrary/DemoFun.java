9068e0a4d3130b6ee75b80ca31a3592941da2c60
vinh.huynh <vinh.huynh@asiantech.vn>
2017-03-14 09:36:25 +0700

    first init

---
+++ app/src/main/java/coder/victorydst3/com/mylibrary/DemoFun.java
@@ -0,0 +1,15 @@
+package coder.victorydst3.com.mylibrary;
+
+/**
+ * Copyright © 2016 AsianTech inc.
+ * Created by VinhHLB on 3/14/17.
+ */
+
+public final class DemoFun {
+    private DemoFun() {
+    }
+
+    public static String getString() {
+        return "String From Library";
+    }
+}
